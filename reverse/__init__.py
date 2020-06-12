2cafe03e6d7349231f94c0504bad81890b946041
Aaron Beasley <aaroncb@aaroncb.com>
2020-06-12 10:46:38 -0500

    ADD Reverse
    - Added Node && LinkedList Classes
    - Added get_value && get_next && set_next to Node
    - Added add_to_head && contains && reverse_list to LinkedList

---
+++ reverse/__init__.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_to_head(self, value):
+        node = Node(value)
+        if self.head is not None:
+            node.set_next(self.head)
+        self.head = node
+
+    def contains(self, value):
+        if not self.head:
+            return False
+        current: Node = self.head
+        while current:
+            if current.get_value() == value:
+                return True
+            current = current.get_next()
+        return False
+
+    def reverse_list(self, node, prev):
+        prev = None
+        current: Node = self.head
+        while current:
+            _next = current.next_node
+            current.next_node = prev
+            prev = current
+            current = _next
+        self.head = prev
